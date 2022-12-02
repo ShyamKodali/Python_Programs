e3325eeed588d46b1ba7c1828cfd9def58b2974c
ShyamKodali <shyamkumarkodali@gmail.com>
2022-12-02 23:43:59 +1100

    basics

---
+++ basic_programs/guess_a_number_game.py
@@ -0,0 +1,19 @@
+secret_number = 624
+
+welcome_text = 'Welcome to the Guess Game!'
+print(welcome_text.center(90))
+
+instructions = 'Enter a number from [1 to 9] in 3 chances'
+print(instructions.center(90))
+
+guess_limit = 3
+guess_count = 0
+
+while guess_count < guess_limit:
+    guess_count += 1
+    guess = int(input("Enter a guess number: "))
+    if guess == secret_number:
+        print("You Won!")
+        break
+else:
+    print("You Failed!")
---
+++ basic_programs/largest number.py
@@ -0,0 +1,8 @@
+numbers = [653, 723, 163, 9303, 3674, 93648, 2764, 72, 8263]
+maxi = numbers[0]
+
+
+for numbers in numbers:
+    if numbers > maxi:
+        maxi = numbers
+print(maxi)
