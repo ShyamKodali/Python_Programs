2a8d7900bcd633e0579a46c14e00fcb585d3cb09
ShyamKodali <shyamkumarkodali@gmail.com>
2023-02-06 19:46:42 +1100

    dict basics

---
+++ basic_programs./dictionary basics.py
@@ -0,0 +1,44 @@
+# Dict Basics 
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Using get method to get value 
+
+print(car.get("model"))
+
+# Updating the value of key and Printing
+
+car["year"] = 2020
+print(car.get("year"))
+
+# Adding New Key/Value pair to the Dict
+
+car["color"]= "red"
+print(car)
+
+# Using pop method
+
+car.pop("model")
+
+# Using clear method
+
+car.clear()
+print(car)
+
+# Using update method
+
+car.update({
+  "brand": "Mazda",
+  "model": "CX5",
+  "year": 2015
+})
+print(car)
+
+# Using pop method 
+
+print(car.pop("year"))
+print(car)
