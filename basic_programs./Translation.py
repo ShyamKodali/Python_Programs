cebd5942641dad7861d827bc18a7751930659d84
ShyamKodali <shyamkumarkodali@gmail.com>
2023-02-18 14:08:05 +1100

    Translation of phrase into j lang

---
+++ basic_programs./Translation.py
@@ -0,0 +1,14 @@
+def translate(phrase):
+    translation = ""
+    
+    for letter in phrase:
+        if letter.lower()in "aeiou":
+            if letter.isupper():
+                translation = translation + "J"
+            else:
+                translation = translation + 'j'
+        else:
+            translation = translation + letter
+    return translation
+            
+print(translate(input("Enter a phrase to transalte in J/j Language: ")))
