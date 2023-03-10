724552d07ebc9ab973f3815c581a56d1bf646b58
ShyamKodali <shyamkumarkodali@gmail.com>
2023-03-10 17:12:51 +1100

    Viewing Data using DataFrame

---
+++ Packages/Pandas/Viewing Data .py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range(start = "2023-03-10", periods=6)
+
+df = pd.DataFrame(np.random.randn(6,6), index = dates, columns=['A1','A2','A3','A4','A5','A6'])
+
+# Viewing Data 
+
+print(df)
+print("**********************************************************************")
+print("Columns of the DataFrame are below: ")
+print(df.columns)
+print("Index of the DataFrame are below: ")
+print(df.index)
+print("**********************************************************************")
+print("First 2 Rows of data are below: ")
+print(df.head(2))
+print("**********************************************************************")
+print("Last 2 Rows of data are below: ")
+print(df.tail(2))
+print("****************************   END   *********************************")
