5b0ef682b5396f0b01bc62303f13141b9f92b030
ShyamKodali <shyamkumarkodali@gmail.com>
2023-03-15 14:54:53 +1100

    Summary Analysis using Pivot Table

---
+++ Packages/Pandas/SA - Pivot Table.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({'A': ['John', 'Boby', 'Mina', 'Peter', 'Nicky'],
+      'B': ['Masters', 'Graduate', 'Graduate', 'Masters', 'Graduate'],
+      'C': [27, 23, 21, 23, 24]})
+
+# Simplest pivot table must have a dataframe
+# and an index/list of index.
+
+tab = pd.pivot_table(df, index =['A', 'B'])
+
+# Creates a pivot table dataframe
+
+table = pd.pivot_table(df, values ='A', index =['B', 'C'],
+                         columns =['B'], aggfunc = np.sum)
+
+
+print(df)
+print("*****************************")
+print(tab)
+print("*****************************")
+print(table)
