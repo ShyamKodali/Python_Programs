54c44a943570a7b8bd48ef788a9fae0b2ebf466f
ShyamKodali <shyamkumarkodali@gmail.com>
2023-07-25 12:36:46 +1000

    recording voice using python

---
+++ projects/Voice_Recorder.py
@@ -0,0 +1,16 @@
+import sounddevice 
+import scipy 
+from scipy.io.wavfile import write
+
+# Sample Rate 
+fs = 44100
+
+# Input Recording Time 
+
+time = int(input("Enter the time (in seconds) for Recording: "))
+print("Recording started......\n")
+record = sounddevice.rec(int(time * fs), samplerate=fs,channels=1)
+sounddevice.wait()
+write("MyRecording.wav", fs, record)
+print("Recording is done please check your folder to listen recording")
+
